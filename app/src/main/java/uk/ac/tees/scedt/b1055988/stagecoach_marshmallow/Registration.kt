024b82e15cc7287e4ccd2e864b620b1dcd45a3f8
Leoconopoly <98611409+Leoconopoly@users.noreply.github.com>
2023-05-16 20:19:27 +0100

    Initial commit

---
+++ app/src/main/java/uk/ac/tees/scedt/b1055988/stagecoach_marshmallow/Registration.kt
@@ -0,0 +1,11 @@
+package uk.ac.tees.scedt.b1055988.stagecoach_marshmallow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
