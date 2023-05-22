0fa88d141e4eb2a3e2350f70a0eb0b1fe3933647
Leoconopoly <98611409+Leoconopoly@users.noreply.github.com>
2023-05-22 22:24:12 +0100

    Registration code implemented

--- app/src/main/java/uk/ac/tees/scedt/b1055988/stagecoach_marshmallow/Registration.kt
+++ app/src/main/java/uk/ac/tees/scedt/b1055988/stagecoach_marshmallow/Registration.kt
@@ -1,11 +1,105 @@
 package uk.ac.tees.scedt.b1055988.stagecoach_marshmallow
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class Registration : AppCompatActivity() {
+
+    private lateinit var editTextEmail: TextInputEditText
+    private lateinit var editTextPassword: TextInputEditText
+    private lateinit var buttonReg: Button
+    private lateinit var progressBar: ProgressBar
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+        mAuth = FirebaseAuth.getInstance()
+
+        editTextEmail = findViewById(R.id.email)
+        editTextPassword = findViewById(R.id.password)
+        buttonReg = findViewById(R.id.btn_register)
+        progressBar = findViewById(R.id.progressBar)
+        textView = findViewById(R.id.loginNow)
+        textView.setOnClickListener {
+            val intent = Intent(applicationContext, Login::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        buttonReg.setOnClickListener {
+            showProgressBar()
+
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+
+            if (TextUtils.isEmpty(email)){
+                Toast.makeText(this@Registration, "Enter email", Toast.LENGTH_SHORT).show()
+                hideProgressBar()
+                return@setOnClickListener
+            }
+
+            if (TextUtils.isEmpty(password)){
+                Toast.makeText(this@Registration, "Enter password", Toast.LENGTH_SHORT).show()
+                hideProgressBar()
+                return@setOnClickListener
+            }
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this,
+                    OnCompleteListener<AuthResult?> { task ->
+                        hideProgressBar()
+
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                this@Registration, "Account Created.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                            Toast.makeText(
+                                this@Registration, "Authentication failed.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    })
+        }
+    }
+
+    private fun showProgressBar() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        progressBar.visibility = View.GONE
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        // Update the UI based on the signed-in user
+        // You can add your own implementation here
+    }
+
+    companion object {
+        private const val TAG = "Registration"
     }
 }
+
+
+
+
+
