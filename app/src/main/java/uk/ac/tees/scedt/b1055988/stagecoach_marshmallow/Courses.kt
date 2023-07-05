2ecc708951366247a23482b4227eaf8311e4948a
Leoconopoly <98611409+Leoconopoly@users.noreply.github.com>
2023-07-05 14:17:24 +0100

    Comments added to Courses.kt

--- app/src/main/java/uk/ac/tees/scedt/b1055988/stagecoach_marshmallow/Courses.kt
+++ app/src/main/java/uk/ac/tees/scedt/b1055988/stagecoach_marshmallow/Courses.kt
@@ -18,33 +18,45 @@ class Courses : AppCompatActivity() {
 
         binding = ActivityCoursesBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Get a reference to the "courses" node in the Firebase Realtime Database
         database = FirebaseDatabase.getInstance().getReference("courses")
+
+        // Initialize the courseList and adapter
         courseList = ArrayList()
         adapter = CourseAdapter(courseList)
 
+        // Set up the RecyclerView with LinearLayoutManager and the adapter
         binding.coursesRecyclerView.layoutManager = LinearLayoutManager(this)
         binding.coursesRecyclerView.adapter = adapter
 
+        // Set up ValueEventListener to listen for changes in the "courses" node
         database.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
+                // Clear the courseList before adding updated data
                 courseList.clear()
+
+                // Iterate through each child snapshot in the "courses" node
                 for (courseSnapshot in snapshot.children) {
+                    // Deserialize the courseSnapshot to a Course object
                     val course = courseSnapshot.getValue(Course::class.java)
                     course?.let { courseList.add(it) }
                 }
 
-                // Sort the courseList based on the date
+                // Sort the courseList based on the date in descending order
                 courseList.sortByDescending { it.date }
 
+                // Notify the adapter of the updated data
                 adapter.notifyDataSetChanged()
             }
 
             override fun onCancelled(error: DatabaseError) {
-                // Handle possible errors.
+                // Handle possible errors or cancellation of the database operation
             }
         })
     }
 }
+
 
 
 
